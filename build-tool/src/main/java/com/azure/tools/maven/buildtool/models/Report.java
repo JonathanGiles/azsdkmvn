f7692b950ec2c5eeb5435685ac4ab58e156fe277
Srikanta <srnagar@microsoft.com>
2021-05-13 23:51:10 -0700

    Initial json report

--- build-tool/src/main/java/com/azure/tools/maven/buildtool/models/Report.java
+++ build-tool/src/main/java/com/azure/tools/maven/buildtool/models/Report.java
@@ -3,35 +3,37 @@ package com.azure.tools.maven.buildtool.models;
 import com.azure.tools.maven.buildtool.mojo.AzureSdkMojo;
 import com.azure.tools.maven.buildtool.util.AnnotatedMethodCallerResult;
 import com.azure.tools.maven.buildtool.util.MojoUtils;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.DependencyManagement;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static com.azure.tools.maven.buildtool.util.MojoUtils.getAllDependencies;
 
 public class Report {
 
     public static final String AZURE_DEPENDENCY_GROUP = "com.azure";
+    private static final String AZURE_SDK_BOM_ARTIFACT_ID = "azure-sdk-bom";
     private final List<String> warningMessages;
     private final List<String> errorMessages;
     private final List<String> failureMessages;
 
-    private String bom;
     private List<String> azureDependencies;
-//    private List<String> consumedServiceMethods;
+    //    private List<String> consumedServiceMethods;
     private Set<AnnotatedMethodCallerResult> serviceMethodCalls;
     private Set<String> outdatedDirectDependencies;
     private Set<String> outdatedTransitiveDependencies;
+    private String bomVersion;
+    private String jsonReport;
 
     public Report() {
         this.warningMessages = new ArrayList<>();
@@ -42,6 +44,7 @@ public class Report {
     /**
      * The report is concluded once all the tools are run. The tools are designed to withhold all result until
      * the conclusion of this report, and then it is the duty of this report to provide the result to the user.
+     * @param mojo The plugin mojo
      */
     public void conclude(AzureSdkMojo mojo) {
         if (!warningMessages.isEmpty() && mojo.getLog().isWarnEnabled()) {
@@ -50,8 +53,10 @@ public class Report {
         if (!errorMessages.isEmpty() && mojo.getLog().isErrorEnabled()) {
             errorMessages.forEach(mojo.getLog()::error);
         }
+        this.bomVersion = getBomVersion(mojo);
         this.azureDependencies = getAzureDependencies(mojo);
-        this.serviceMethodCalls = getServiceMethodCalls(mojo);
+
+        createJsonReport(mojo);
         // we throw a single runtime exception encapsulating all failure messages into one
         if (!failureMessages.isEmpty()) {
             StringBuilder sb = new StringBuilder("Build failure for the following reasons:\n");
@@ -60,21 +65,79 @@ public class Report {
         }
     }
 
-    private Set<AnnotatedMethodCallerResult> getServiceMethodCalls(AzureSdkMojo mojo) {
-        // TODO
-        return Collections.emptySet();
+    private String getBomVersion(AzureSdkMojo mojo) {
+        DependencyManagement depMgmt = mojo.getProject().getDependencyManagement();
+        Optional<Dependency> bomDependency = Optional.empty();
+        if (depMgmt != null) {
+            bomDependency = depMgmt.getDependencies().stream()
+                    .filter(d -> d.getArtifactId().equals(AZURE_SDK_BOM_ARTIFACT_ID))
+                    .findAny();
+        }
+
+        if (bomDependency.isPresent()) {
+            return bomDependency.get().getVersion();
+        }
+        return null;
     }
 
+    private void createJsonReport(AzureSdkMojo mojo) {
 
-    private Stream<Path> getAllSourceFiles(AzureSdkMojo mojo, Object sourceRootDir) {
         try {
-            return Files.walk(Paths.get(sourceRootDir.toString()))
-                    .filter(Files::isRegularFile)
-                    .filter(file -> file.toFile().getAbsolutePath().endsWith(".java"));
-        } catch (Exception exception) {
-            mojo.getLog().info("Exception while visiting files " + exception.getMessage());
-            return Stream.empty();
+            StringWriter writer = new StringWriter();
+            JsonGenerator generator = new JsonFactory().createGenerator(writer);
+
+            generator.writeStartObject();
+            generator.writeStringField("group", mojo.getProject().getGroupId());
+            generator.writeStringField("artifact", mojo.getProject().getArtifactId());
+            generator.writeStringField("version", mojo.getProject().getVersion());
+            generator.writeStringField("name", mojo.getProject().getName());
+            if (this.bomVersion != null && !this.bomVersion.isEmpty()) {
+                generator.writeStringField("bomVersion", this.bomVersion);
+            }
+            if (this.azureDependencies != null && !this.azureDependencies.isEmpty()) {
+                writeArray("azureDependencies", azureDependencies, generator);
+            }
+
+            if (this.outdatedDirectDependencies != null && !this.outdatedDirectDependencies.isEmpty()) {
+                writeArray("outdatedDependencies", outdatedDirectDependencies, generator);
+            }
+
+            if (this.serviceMethodCalls != null && !this.serviceMethodCalls.isEmpty()) {
+                writeArray("serviceMethodCalls", serviceMethodCalls
+                        .stream()
+                        .map(AnnotatedMethodCallerResult::toString)
+                        .collect(Collectors.toList()), generator);
+            }
+
+            if (this.errorMessages != null && !this.errorMessages.isEmpty()) {
+                writeArray("errorMessages", errorMessages, generator);
+            }
+
+            if (this.warningMessages != null && !this.warningMessages.isEmpty()) {
+                writeArray("warningMessages", warningMessages, generator);
+            }
+
+            if (this.failureMessages != null && !this.failureMessages.isEmpty()) {
+                writeArray("failureMessages", failureMessages, generator);
+            }
+
+            generator.writeEndObject();
+            generator.close();
+            writer.close();
+
+            this.jsonReport = writer.toString();
+        } catch (IOException exception) {
+
         }
+    }
+
+    private void writeArray(String fieldName, Collection<String> values, JsonGenerator generator) throws IOException {
+        generator.writeFieldName(fieldName);
+        generator.writeStartArray();
+        for (String value : values) {
+            generator.writeString(value);
+        }
+        generator.writeEndArray();
     }
 
     private List<String> getAzureDependencies(AzureSdkMojo mojo) {
